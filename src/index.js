dffa77c4e9603f11f9ef24e28701d422aa091a8c
Andrés Muñoz <andres2078@gmail.com>
2026-06-26 17:31:37 -0500

    chore: fix unused import warning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axiosSetup from './axiosSetup';
+import './axiosSetup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
